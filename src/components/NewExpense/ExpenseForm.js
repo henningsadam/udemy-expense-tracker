e53f785f84b6bf15d6be84d6e8cbba7b70dad4fb
Adam Hennings <hennings.adam@gmail.com>
2023-10-16 10:32:42 -0600

    added the ability to forward a newly added expense to the app component

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,24 +1,75 @@
-import React from 'react';
-import './ExpenseForm.css'
+import React, { useState } from 'react';
+import './ExpenseForm.css';
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+
+  const inputChangeHandler = (id, value) => {
+    if (id === 'title') {
+      setEnteredTitle(value);
+    } else if (id === 'date') {
+      setEnteredDate(value);
+    } else {
+      setEnteredAmount(value);
+    }
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+        title: enteredTitle,
+        amount: enteredAmount,
+        date: new Date(enteredDate)
+    }
+
+    props.onSaveExpenseData(expenseData)
+    setEnteredTitle('');
+    setEnteredDate('');
+    setEnteredAmount('');
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' />
+          <input
+            type='text'
+            value={enteredTitle}
+            onChange={(event) => {
+              inputChangeHandler('title', event.target.value);
+            }}
+          />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' min='0.01' step='0.01'/>
+          <input
+            type='number'
+            min='0.01'
+            step='0.01'
+            value={enteredAmount}
+            onChange={(event) => {
+              inputChangeHandler('amount', event.target.value);
+            }}
+          />
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='date' min='2019-01-01' step='2022-12-31'/>
+          <input
+            type='date'
+            value={enteredDate}
+            min='2019-01-01'
+            step='2022-12-31'
+            onChange={(event) => {
+              inputChangeHandler('date', event.target.value);
+            }}
+          />
         </div>
       </div>
-      <div className="new-expense__actions">
+      <div className='new-expense__actions'>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
